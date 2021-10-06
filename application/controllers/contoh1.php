e2ac23c2c604e66f77fe3f3edecc9c50dc5aa317
WP2-putradasu <putradasu.pd@gmail.com>
2021-10-07 04:29:15 +0700

    penambahan application

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Vincentius M D Putra Dasu <br>
+            tinggal di Sleman,hobby main game";
+ }
+}
